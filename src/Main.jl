379929975d3a71551bad5e27ce449520d96a0703
Franck Marnix <Marnix.Franck@mil.be>
2025-04-04 19:17:40 +0200

    voor u package GLMakie te laten installeren

--- src/Main.jl
+++ src/Main.jl
@@ -7,3 +7,15 @@ include("UI.jl")
 using .board 
 using .piece
 using .ui
+
+
+	this_file = split(basename(@__FILE__), '#')[1]
+    import Pkg
+    Pkg.activate(io = IOBuffer())
+	deps = [pair.second for pair in Pkg.dependencies()]
+	direct_deps = filter(p -> p.is_direct_dep, deps)
+    pkgs = [x.name for x in direct_deps]
+	if "GLMakie" ∉ pkgs
+		Pkg.add(GLMakie)
+	end
+	using GLMakie
