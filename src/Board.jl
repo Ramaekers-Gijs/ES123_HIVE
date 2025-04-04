61231d94e02d9ef344a96fbe7add3c48587c078d
Ramaekers Gijs <Gijs.Ramaekers@mil.be>
2025-04-04 17:07:32 +0200

    m

--- src/Board.jl
+++ src/Board.jl
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
 module board
 end
-=======
 
->>>>>>> 18a30d5693ceaf3b0c7a4967f2dc0cc81bd0ba1f
--- src/Piece.jl
+++ src/Piece.jl
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
 module piece 
 end
-=======
-
->>>>>>> 18a30d5693ceaf3b0c7a4967f2dc0cc81bd0ba1f
