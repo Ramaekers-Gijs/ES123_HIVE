3a6784d1e6f2380b28ee297e2addbea662fb07d4
Gijs Ramaekers <77696565+Ramaekers-Gijs@users.noreply.github.com>
2025-04-06 10:42:48 +0200

    Update Main.jl

--- src/Main.jl
+++ src/Main.jl
@@ -1,4 +1,15 @@
+""" this_file = split(basename(@__FILE__), '#')[1]
+import Pkg
+Pkg.activate(io = IOBuffer())
+deps = [pair.second for pair in Pkg.dependencies()]
+direct_deps = filter(p -> p.is_direct_dep, deps)
+pkgs = [x.name for x in direct_deps]
+if "GLMakie" ∉ pkgs
+    Pkg.add(GLMakie)
+end"""
+
 using GLMakie 
+using Revise
 
 include("Board.jl")
 include("Piece.jl")
@@ -7,15 +18,3 @@ include("UI.jl")
 using .board 
 using .piece
 using .ui
-
-
-	this_file = split(basename(@__FILE__), '#')[1]
-    import Pkg
-    Pkg.activate(io = IOBuffer())
-	deps = [pair.second for pair in Pkg.dependencies()]
-	direct_deps = filter(p -> p.is_direct_dep, deps)
-    pkgs = [x.name for x in direct_deps]
-	if "GLMakie" ∉ pkgs
-		Pkg.add(GLMakie)
-	end
-	using GLMakie
