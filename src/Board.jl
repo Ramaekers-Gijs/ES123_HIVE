d8b118183b5496917eec9af092be2c4dd58ff539
Ramaekers-Gijs <gijs.ramaekers@live.be>
2025-04-06 13:29:01 +0200

    hexagons

--- src/Board.jl
+++ src/Board.jl
@@ -7,33 +7,41 @@ HEIGHT = 1080
 BACKGROUND = colorant"antiquewhite"
 
 # define initial state of actors
-struct HexGridPoint 
+struct HexCoord 
     x::Int64
     y::Int64
 end 
 
-
-
-function points_hex(center::HexGridPoint;size=50.0)
-    theta = LinRange(0,2*pi,7) 
-    #iseven(center.x) ? offset = 0 : offset = size 
-    x = center.x .+ cos.(theta) .* size
-    y = center.y .+ sin.(theta) .* size #.+ offset
-    return collect(zip(x, y))  # Return as list of (x, y) tuples for drawpoly
+function HexCoord2AbsCoord(HexCoord::HexCoord)
+    x = 1035 + 75 * HexCoord.x
+    iseven(HexCoord.x) ? y = floor(Int,540 + sqrt(3)*25*2*HexCoord.y) : y = floor(Int,540 + sqrt(3)*25*(2*HexCoord.y+1))
+    return x,y
 end
-pnt =  points_hex(HexGridPoint(300,300))
-Line(round(pnt[1][1]),round(pnt[1][2]),round(pnt[2][1]),round(pnt[2][2]))
 
-function drawhex(points)
-    # Draw lines between each pair of points
-    for i in 1:length(points)-1
-        draw(Line(round(pnt[i][1]),round(pnt[i][2]),round(pnt[i+1][1]),round(pnt[i+1][2])))
+function DrawHex(center::HexCoord)
+    theta = LinRange(0,2*pi,7) 
+
+    x = floor.(Int,HexCoord2AbsCoord(center)[1] .+ cos.(theta) .* 50)
+    y = floor.(Int,HexCoord2AbsCoord(center)[2] .+ sin.(theta) .* 50)
+
+    for i in 1:length(x)-1
+        draw(Line(x[i],y[i],x[i+1],y[i+1]))
     end
     # Connect the last point to the first to close the shape
-    draw(Line(round(pnt[end][1]),round(pnt[end][2]),round(pnt[1][1]),round(pnt[1][2])))
+    draw(Line(x[end],y[end],x[1],y[1]))
 end
+
+function DrawGrid(x,y)
+    dx = floor(x/2)
+    dy = floor(y/2)
+    for i in -dx:dx
+        for j in -dy:dy
+        DrawHex(HexCoord(i,j))
+        end   
+    end 
+end
+
 # GameZero draw function
 function draw(g::Game)
-    pts = points_hex(HexGridPoint(300, 300))
-    drawhex(pts)
+    DrawGrid(7,7)
 end
