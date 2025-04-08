8f5fd71362fc26813f029f18361cbf175d7b4d1c
Ramaekers-Gijs <gijs.ramaekers@live.be>
2025-04-08 15:28:08 +0200

    m

--- src/Board.jl
+++ src/Board.jl
@@ -1,4 +1,4 @@
-using GameZero
+using GameZero, Colors
 
 game[].location = @__DIR__
 
@@ -6,7 +6,7 @@ WIDTH = 1920
 HEIGHT = 700
 BACKGROUND = colorant"antiquewhite"
 
-struct HexCoord 
+struct HexCoord #overbodig
     x::Int
     y::Int
 end 
@@ -16,13 +16,15 @@ struct Piece
     player::Int #1 of 2
 end
 
+Bord = Dict{HexCoord, Piece}()
+
 function HexCoord2AbsCoord(HexCoord::HexCoord)
     x = 1035 + 75 * HexCoord.x
     iseven(HexCoord.x) ? y = trunc(Int,540 + sqrt(3)*(-50)*HexCoord.y) : y = trunc(Int,540 + sqrt(3)*25*(-2*HexCoord.y-1))
     return x,y
 end
 
-function DrawHex(center::HexCoord)
+function DrawHex(center::HexCoord) #kijken of gaat zoals in package
     theta = LinRange(0,2*pi,7) 
 
     x = trunc.(Int,HexCoord2AbsCoord(center)[1] .+ cos.(theta) .* 50)
@@ -45,7 +47,7 @@ function DrawGrid(x,y)
     end 
 end
 
-function approx(x,y)
+function approx(x,y) #kanmet collide
     approxx = floor(Int,(x-1035+50-12)/75)
     iseven(approxx) ? approxy = floor(Int,(-y+540+sqrt(3)*25)/(50*sqrt(3))) : approxy = floor(Int,(-y+540)/(50*sqrt(3)))
     return HexCoord(approxx,approxy)
@@ -53,31 +55,47 @@ end
 
 
 function on_mouse_down(g::Game,pos) #can be done with squares is easier but less precise
-    ap = approx(pos[1],pos[2])
-    AbsAp = HexCoord2AbsCoord(ap) #approximation can be wrong because of the hexagonal shape of the tiles 
-    dist = sqrt((AbsAp[1]-pos[1])^2+(AbsAp[2]-pos[2])^2) 
-    if dist < 25*sqrt(3)
-       println(string(ap.x)*" "*string(ap.y))
+    if pos[1] > 200 
+        #ClickInField(pos)
+    elseif pos[1] < 200
+        #ClickOutField()
     end
 end
 
-variable = Actor("1.png")
-#variable.center = (1035,540)
+function ClickInField(pos)
+    ap = approx(pos[1],pos[2])
+    apAbs = HexCoord2AbsCoord(ap)
+    dist = sqrt((pos[1]-apAbs[1])^2+(pos[2]-apAbs[2])^2)
+    if dist < 25 * sqrt(3)
+        if haskey(Bord,ap)
+            Move(ap,key(ap)) #if there is a piece there
+        else 
+            EmptySpaceUI(ap) #if there is no piece there 
+        end
+    end
+end
 
+function Move(HexCoord::HexCoord,Piece::Piece)
+    PosMoves = CanMove(HexCoord::HexCoord,Piece::Piece)
+    EndPos = makeclickable(PosMoves)
+    place(EndPos)
+end
 
-function PlacePiece()
-    variable = Actor("1.png")
-    variable.center=(1035,540)
-    draw(variable)
+function place(HexCoord::HexCoord,Piece::Piece)
+    if mod(Piece.player, 10) == 1 
+        draw(Rect((1000,520),(100,100)), colorant"red",fill=true) #draw black Hex 
+    else
+        #draw white Hex 
+    end
+    a = HexCoord2AbsCoord(HexCoord)
+    pic = Actor(string(Piece.soort)*".png", scale=[1/8,1/8], position =  Rect(a[1]-32,a[2]-32,512,512)) #de ingebouwde functie center gaat kapot bij scaling
+    draw(pic)
 end
 
 # GameZero draw function
 function draw(g::Game)
-    PlacePiece()
+    place(HexCoord(0,0),Piece(1,1))
     DrawGrid(10,10)
     draw(Line(0,540,1920,540)) #not permantent for testing
     draw(Line(1035,0,1035,1080))#not permantent for testing
-end
-
-function EmptySpaceUI(player)
 end
