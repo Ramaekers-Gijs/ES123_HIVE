e8e246c829fbd83b5213fb7883f20cb8671bb6f3
Ramaekers-Gijs <gijs.ramaekers@live.be>
2025-04-10 22:43:58 +0200

    update

--- src/Board.jl
+++ src/Board.jl
@@ -5,26 +5,29 @@ game[].location = @__DIR__
 WIDTH = 1920
 HEIGHT = 700
 BACKGROUND = colorant"antiquewhite"
-
+"""
 struct HexCoord #overbodig
     x::Int
     y::Int
 end 
-
+"""
 struct Piece
     soort::Int # 1 - 5 1:ant 2:beetle 3:grasshopper 4:queen bee 5:spider (op elkaar ziet er zo uit 123)
     player::Int #1 of 2
 end
 
-Bord = Dict{HexCoord, Piece}()
+Bord = Dict{Tuple{Int, Int}, Piece}()
 
-function HexCoord2AbsCoord(HexCoord::HexCoord)
+const directions_even = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, -1), (-1, 0)]
+const directions_uneven = [(0, 1), (1, 1), (1, 0), (0, -1), (-1, 0), (-1, 1)]
+
+function HexCoord2AbsCoord(HexCoord::Tuple{Int, Int}) #input in hexcoord #af
     x = 1035 + 75 * HexCoord.x
     iseven(HexCoord.x) ? y = trunc(Int,540 + sqrt(3)*(-50)*HexCoord.y) : y = trunc(Int,540 + sqrt(3)*25*(-2*HexCoord.y-1))
     return x,y
 end
 
-function DrawHex(center::HexCoord) #kijken of gaat zoals in package
+function DrawHex(center::Tuple{Int, Int}) #input in hexcoord #af
     theta = LinRange(0,2*pi,7) 
 
     x = trunc.(Int,HexCoord2AbsCoord(center)[1] .+ cos.(theta) .* 50)
@@ -37,7 +40,7 @@ function DrawHex(center::HexCoord) #kijken of gaat zoals in package
     draw(Line(x[end],y[end],x[1],y[1]))
 end
 
-function DrawGrid(x,y)
+function DrawGrid(x,y)#af
     dx = floor(x/2)
     dy = floor(y/2)
     for i in -dx:dx
@@ -47,7 +50,7 @@ function DrawGrid(x,y)
     end 
 end
 
-function approx(x,y) #kanmet collide
+function approx(x,y) #kanmet collide #af
     approxx = floor(Int,(x-1035+50-12)/75)
     iseven(approxx) ? approxy = floor(Int,(-y+540+sqrt(3)*25)/(50*sqrt(3))) : approxy = floor(Int,(-y+540)/(50*sqrt(3)))
     return HexCoord(approxx,approxy)
@@ -79,12 +82,12 @@ function ClickInField(pos)
     end
 end
 
-function Move(HexCoord::HexCoord,Piece::Piece)
+function Move(HexCoord::Tuple{Int, Int},Piece::Piece) #input in hexcoord
     PosMoves = CanMove(HexCoord::HexCoord,Piece::Piece)
     makeclickable(PosMoves)
 end
 
-function CanMove(HexCoord::HexCoord,Piece::Piece)
+function CanMove(HexCoord::Tuple{Int, Int},Piece::Piece) #input in hexcoord
     if Piece.soort == 1
         array = CanMoveAnt(HexCoord::HexCoord,Piece::Piece)
     elseif Piece.soort == 2
@@ -97,10 +100,6 @@ function CanMove(HexCoord::HexCoord,Piece::Piece)
         array = CanMoveSpider(HexCoord::HexCoord,Piece::Piece)
     end
     return array
-end
-
-function CanMoveQueen(HexCoord::HexCoord,Piece::Piece)
-    
 end
 
 function place(HexCoord::HexCoord,Piece::Piece)
@@ -116,10 +115,7 @@ function place(HexCoord::HexCoord,Piece::Piece)
     #draw(pic) comment export the variable so that it can be called in the draw function 
 end
 
-function is_hive_connected(board::Dict{Tuple{Int, Int}, String}, exclude::Tuple{Int, Int})
-    # Hex directions
-    directions = [(0, 1), (1, 0), (1, -1), (0, -1), (-1, 0), (-1, 1)]
-
+function is_hive_connected(board::Dict{Tuple{Int, Int}, String}, exclude::Tuple{Int, Int}) #input in hexcoord
     # Copy board and remove the piece we're simulating as removed
     temp_board = Dict(board)
     delete!(temp_board, exclude)
@@ -135,18 +131,30 @@ function is_hive_connected(board::Dict{Tuple{Int, Int}, String}, exclude::Tuple{
     while !isempty(stack)
         current = pop!(stack)
         push!(visited, current)
-        @info visited 
+        iseven(current[1]) ? directions = directions_even : directions = directions_uneven
         for (dx, dy) in directions
             neighbor = (current[1] + dx, current[2] + dy)
-            @info neighbor
             if haskey(temp_board, neighbor) && !(neighbor in visited)
                 push!(stack, neighbor)
             end
         end
     end
-    @info visited
-    @info positions
     return length(visited) == length(positions)
+end
+
+function is_slidable(from::Tuple{Int, Int}, to::Tuple{Int, Int}) #input in hexcoord
+    dx = to[1] - from[1] 
+    dy = to[2] - from[2]
+    iseven(from[1]) ? directions = directions_even : directions = directions_uneven
+    i = findfirst(d -> d == (dx, dy), directions) #try catch
+
+    left = directions[mod1(i - 1, 6)]
+    right = directions[mod1(i + 1, 6)]
+
+    left_pos = (from[1] + left[1], from[2] + left[2])
+    right_pos = (from[1] + right[1], from[2] + right[2])
+
+    return !(haskey(board, left_pos) && haskey(board, right_pos))
 end
 
 # GameZero draw function
